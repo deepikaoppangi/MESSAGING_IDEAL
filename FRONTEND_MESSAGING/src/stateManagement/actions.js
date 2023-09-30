40fe11ce9a2c716930af5f8ae3a5e772a5353f91
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2023-09-30 15:40:38 +0530

    Delete FRONTEND_MESSAGING directory

--- FRONTEND_MESSAGING/src/stateManagement/actions.js
+++
@@ -1,5 +0,0 @@
-export const updateChatAction = "updateChatAction";
-export const userDetailAction = "userDetailAction";
-export const activeChatAction = "activeChatAction";
-export const activeChatUserAction = "activeChatUserAction";
-export const triggerRefreshUserListAction = "triggerRefreshUserListAction";
